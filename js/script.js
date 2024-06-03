3f337467e54f16133974c128695bc44be2482735
Shamlov <pshamlov@gmail.com>
2024-06-03 21:39:43 +0300

    V2

--- js/script.js
+++ js/script.js
@@ -48,48 +48,39 @@ btn.addEventListener('click', addLike)
 // НЕ УДАЛОСЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 let dataOutput = document.querySelector('#dataOutput')
 let keyboardContainer = document.querySelector('#keyboardContainer')
-
-
 function getOutputResult (e) {  
-    let number
-    let operation
-    let number1 = undefined
+    let number                      // получаю каждую цифру при клике
+    let operation                  // знак опереции
+    let number1 = undefined        //  получаю число целиком
     let rez
-
-    if(!e.target.closest('span')) {   
+    if(!e.target.closest('span')) {     // делегирование
         return
     }
-    let regexp = new RegExp ('\\d', 'gi')   //   создадим резулярное выражение для отсева цифр
-
+    let regexp = new RegExp ('\\d', 'gi')   //   создадим резулярное выражение для отсева цифр 
     if(regexp.test(e.target.dataset.value)) {     // отсортеруем цифры и знаки
         number = e.target.dataset.value
     } else {
         operation = e.target.dataset.value
     }
-
-    if(number != undefined) {
+    if(number != undefined) {                   // сохраним число 
         dataOutput.textContent += number
     } else {
         number1 = parseInt(dataOutput.textContent)
     }
-
-    if(operation == '+' && '-' && '*' && '/'&& '='){
+    if(operation == '+' && '-' && '*' && '/'&& '='){   // при клике на символ сохраним число в результат для  того чтобы не плодить переменные 
         rez = number1
     }
-
+    // код ниже не могу собрать . хотя решение где то на поверхности
     if(number != undefined) {
         dataOutput.textContent += number
     } else {
         number1 = parseInt(dataOutput.textContent)
     }
-
     if(operation == '+') {
         console.log(rez)
         console.log(number1)
-    }
-
+    }_
 }
-
 keyboardContainer.addEventListener('click', getOutputResult)
 
 // не удалось.
@@ -101,6 +92,14 @@ keyboardContainer.addEventListener('click', getOutputResult)
 списки. Список с элементами подменю должен появляться по 
 щелчку на соответствующий элемент меню.*/
 
+let menu = document.querySelector('#menu')
+menu.addEventListener('click', (e) => {
+    if(!e.target.closest('li')) {     // делегирование
+        return
+    }
+    e.target.firstElementChild.classList.toggle('closed')   // тут примения универсальность и обращаюсь к предку элемента на который кликнули. 
+    
+})
 
 
-
+//////////////////////////////////////////////////////////////
